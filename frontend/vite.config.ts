67d463d1e7e62d2cf18367718628cc84a578b3ef
chibx <111882496+chibx@users.noreply.github.com>
2025-09-11 17:56:58 +0100

    Minor change

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,22 @@
+import { PrimeVueResolver } from '@primevue/auto-import-resolver'
 import tailwindcss from '@tailwindcss/vite'
-import { fileURLToPath, URL } from 'node:url'
-
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 const { GO_PORT, GO_HOST } = process.env
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [
+    vue(),
+    // vueDevTools(),
+    tailwindcss(),
+    Components({
+      resolvers: [PrimeVueResolver({ components: { prefix: 'PrimeVue' } })],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),

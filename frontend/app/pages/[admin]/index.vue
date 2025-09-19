251c68af425cf9ed8187fac8c435fd2dd4435e54
chibx <111882496+chibx@users.noreply.github.com>
2025-09-19 17:30:49 +0100

    Just testing something
    Nothing much

--- frontend/app/pages/[admin]/index.vue
+++ frontend/app/pages/[admin]/index.vue
@@ -1,12 +1,15 @@
 <script lang="ts" setup>
 // import Button from "primevue/button";
-
 </script>
 
 <template>
   <div>
-    <h1>Yo wassup</h1>
+    <h1 class="bg-red-900 text-white font-bold p-10">Yo wassup</h1>
     <PrimeButton color="blue">Fuck you</PrimeButton>
+    <PrimePassword></PrimePassword>
+    <div>
+      <PrimeAvatar />
+    </div>
   </div>
 </template>
 

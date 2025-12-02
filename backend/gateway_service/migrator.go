bd52cab41a30a6a2a2ceb92346ea68823ffcd953
chibx <111882496+chibx@users.noreply.github.com>
2025-12-02 11:56:16 +0100

    New Commit

--- backend/gateway_service/migrator.go
+++ backend/gateway_service/migrator.go
@@ -7,12 +7,48 @@ import (
 )
 
 func migrate(db *gorm.DB) error {
+	err := db.SetupJoinTable(&model.Product{}, "Categories", &model.ProductCategoryValues{})
+	if err != nil {
+		return err
+	}
+	err = db.SetupJoinTable(&model.Product{}, "Tags", &model.ProductTags{})
+	if err != nil {
+		return err
+	}
+
 	return db.AutoMigrate(
+		// Important Tables
+		model.AppData{},
+		model.Country{},
+		model.State{},
+		// Backend
 		model.BackendUser{},
+		model.ApiKey{},
+		model.BackendOTP{},
+		model.BackendSession{},
+		model.BackendUserActivity{},
+		// Catalog
 		model.Product{},
+		model.Attribute{},
+		model.Category{},
+		model.Preset{},
+		model.PresetAttributes{},
+		model.ProductCategoryValues{},
+		model.Tag{},
+		model.ProductTags{},
+		model.Order{},
+		model.OrderItem{},
+		model.OrderReturn{},
+		// Customer
 		model.Customer{},
 		model.CustomerOTP{},
 		model.CustomerSession{},
-		model.BackendSession{},
+		model.CartItem{},
+		model.WishlistItem{},
+
+		// Inventory
+		model.Inventory{},
+		model.Warehouse{},
+		model.StockMovement{},
 	)
 }

96d00eeb3977bf1b5342604ddf95f93c7c80ccdd
chibx <111882496+chibx@users.noreply.github.com>
2025-12-05 01:28:50 +0100

    Working on Cache

--- backend/gateway_service/internal/cache/admin.go
+++ backend/gateway_service/internal/cache/admin.go
@@ -1,9 +1,43 @@
 package cache
 
 import (
+	"errors"
+	"vuecom/gateway/internal/cache/keys"
 	"vuecom/gateway/internal/v1/types"
+	dbModels "vuecom/shared/models/db"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/redis/go-redis/v9"
+	"gorm.io/gorm"
 )
 
-func FetchAppData(api *types.Api) {
+func GetAppData(ctx *fiber.Ctx, api *types.Api) (*dbModels.AppData, error) {
+	db := api.Deps.DB
+	cache := api.Deps.Redis
+	context := ctx.Context()
+	appData := new(dbModels.AppData)
 
+	err := cache.Get(context, keys.APP_DATA_KEY).Scan(&appData)
+	if err != nil {
+		if !errors.Is(err, redis.Nil) {
+			return nil, fiber.NewError(fiber.StatusInternalServerError, "Error fetching app data")
+		}
+
+		// Key Not Found
+		if err := db.WithContext(context).Limit(1).First(appData).Error; err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, fiber.NewError(fiber.StatusInternalServerError, "Error fetching app data")
+		}
+
+		if appData == nil {
+			return nil, nil
+		}
+
+		if err := cache.Set(context, keys.APP_DATA_KEY, appData, 0).Err(); err != nil {
+			// TODO: Log cache issue
+			return nil, fiber.NewError(fiber.StatusInternalServerError, "Error fetching app data")
+		}
+
+	}
+
+	return appData, nil
 }
---
+++ backend/gateway_service/internal/cache/keys/keys.go
@@ -0,0 +1,3 @@
+package keys
+
+const APP_DATA_KEY = "app_data"

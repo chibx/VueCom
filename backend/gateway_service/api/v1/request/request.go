4eb0219e623bfff3a36977891d0a78d58631cbb3
chibx <111882496+chibx@users.noreply.github.com>
2025-12-08 00:29:54 +0100

    Added utility functions for working with paths when storing assets in the cloud storage

--- backend/gateway_service/api/v1/request/request.go
+++ backend/gateway_service/api/v1/request/request.go
@@ -1,7 +1,24 @@
 package request
 
+import (
+	"path"
+	"vuecom/gateway/internal/v1/types"
+)
+
 type OnlyID struct {
 	ID int `json:"id" params:"id"`
 }
 
 var IMAGE_FORMATS = []string{"image/jpeg", "image/png", "image/jpg"}
+var GetBackendFolder = func(api *types.Api) string {
+	// return api.AppName + "/backend_users"
+	return path.Join(api.AppName, "backend_users")
+}
+
+var GetCustomerFolder = func(api *types.Api) string {
+	return path.Join(api.AppName, "customers")
+}
+
+var GetProductFolder = func(api *types.Api) string {
+	return path.Join(api.AppName, "products")
+}

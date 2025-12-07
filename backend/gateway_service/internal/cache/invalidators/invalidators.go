48d22fe2ca4c47d86ac1770dda16d621dffd71f2
chibx <111882496+chibx@users.noreply.github.com>
2025-12-08 00:35:34 +0100

    Still working on the caching strategies

--- backend/gateway_service/internal/cache/invalidators/invalidators.go
+++ backend/gateway_service/internal/cache/invalidators/invalidators.go
@@ -1,1 +1,40 @@
 package invalidators
+
+import (
+	"context"
+
+	"github.com/redis/go-redis/v9"
+)
+
+const DEFAULT_BATCH_SIZE = 100
+const USER_KEY_PATTERN = "user:*"
+const PRODUCT_KEY_PATTERN = "product:*"
+const BACKEND_USER_KEY_PATTERN = "backend_user:*"
+
+// TODO: I believe this simple function could be better optimized later
+
+func InvalidateCache(rdb *redis.Client, pattern string) {
+	go func() {
+		var cursor uint64
+		var deletedCount int
+		ctx := context.Background()
+		for {
+			keys, nextCursor, _ := rdb.Scan(ctx, cursor, pattern, DEFAULT_BATCH_SIZE).Result()
+			// if err != nil {
+			// 	return deletedCount, err
+			// }
+			if len(keys) > 0 {
+				_, _ = rdb.Unlink(ctx, keys...).Result()
+				// if err != nil {
+				// 	return deletedCount, err
+				// }
+				deletedCount += len(keys)
+			}
+			cursor = nextCursor
+			if cursor == 0 {
+				break
+			}
+		}
+	}()
+	// return deletedCount, nil
+}
--- backend/gateway_service/internal/cache/keys/keys.go
+++ backend/gateway_service/internal/cache/keys/keys.go
@@ -1,3 +1,20 @@
 package keys
 
+import "strconv"
+
 const APP_DATA_KEY = "app_data"
+const USER_KEY_PREFIX = "user:"
+const PRODUCT_KEY_PREFIX = "product:"
+const BACKEND_USER_KEY_PREFIX = "backend_user:"
+
+func UserKey(userId int) string {
+	return USER_KEY_PREFIX + strconv.Itoa(userId)
+}
+
+func ProductKey(productId int) string {
+	return PRODUCT_KEY_PREFIX + strconv.Itoa(productId)
+}
+
+func BackendUserKey(userId int) string {
+	return BACKEND_USER_KEY_PREFIX + strconv.Itoa(userId)
+}

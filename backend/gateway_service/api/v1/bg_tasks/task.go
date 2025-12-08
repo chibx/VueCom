4303e49e67d8c780179ebc200eb58ed497295f5d
chibx <111882496+chibx@users.noreply.github.com>
2025-12-08 03:00:43 +0100

    New Commit

--- backend/gateway_service/api/v1/bg_tasks/task.go
+++ backend/gateway_service/api/v1/bg_tasks/task.go
@@ -10,10 +10,10 @@ import (
 // This helps to refresh the local copy of the slug from the data fetching function
 func RefreshAdminSlug(api *types.Api) {
 	ticker := time.NewTicker(time.Minute)
-
+	ctx := context.Background()
 	go func() {
 		for range ticker.C {
-			appData, _ := cache.GetAppData(context.Background(), api)
+			appData, _ := cache.GetAppData(ctx, api)
 
 			if appData != nil {
 				api.AdminSlug = appData.AdminRoute

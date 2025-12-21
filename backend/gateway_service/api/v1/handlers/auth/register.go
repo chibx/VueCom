947eb4e08b0aa1d192f6a9e8795b857fe1e4eba4
chibx <111882496+chibx@users.noreply.github.com>
2025-12-21 23:23:46 +0100

    Working on things

--- backend/gateway_service/api/v1/handlers/auth/register.go
+++ backend/gateway_service/api/v1/handlers/auth/register.go
@@ -1,1 +1,19 @@
 package auth
+
+import (
+	"vuecom/gateway/internal/v1/types"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func RegisterRoutes(app fiber.Router, api *types.Api) {
+	auth := app.Group("/auth")
+	auth.Post("/register")
+	auth.Post("/login")
+	auth.Post("/refresh")
+
+	backendAuth := auth.Group("/backend")
+	backendAuth.Post("/register")
+	backendAuth.Post("/login")
+	backendAuth.Post("/refresh")
+}

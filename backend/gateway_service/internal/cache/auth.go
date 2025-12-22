e446f35f029d314b4ab711c11855b798d3d83ae2
chibx <111882496+chibx@users.noreply.github.com>
2025-12-22 21:51:00 +0100

    Cache for session data

--- backend/gateway_service/internal/cache/auth.go
+++ backend/gateway_service/internal/cache/auth.go
@@ -3,10 +3,12 @@ package cache
 import (
 	"context"
 	"errors"
+	"time"
 	"vuecom/gateway/internal/v1/types"
 	userErrors "vuecom/shared/errors/users"
 	dbModels "vuecom/shared/models/db"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/redis/go-redis/v9"
 	"gorm.io/gorm"
 )
@@ -21,20 +23,26 @@ func GetCustomerSession(token string, api *types.Api, context context.Context) (
 
 	if err != nil {
 		if !errors.Is(err, redis.Nil) {
-			return nil, userErrors.NewTokenErr(500, "Something went wrong while getting your session data. Please try again later.")
+			return nil, userErrors.NewTokenErr(fiber.StatusInternalServerError, "Something went wrong while getting your session data. Please try again later.")
 		}
 
 		result := db.WithContext(context).First(cus_session, "token = ?", token)
 		if result.Error != nil {
 			if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-				return nil, userErrors.NewTokenErr(401, "User Session not found. Consider logging in again")
+				return nil, userErrors.NewTokenErr(fiber.StatusUnauthorized, "User Session not found. Consider logging in again")
 			}
 
-			return nil, userErrors.NewTokenErr(500, "Something went wrong while fetching your session data. Please try again later.")
+			return nil, userErrors.NewTokenErr(fiber.StatusInternalServerError, "Something went wrong while fetching your session data. Please try again later.")
 		}
 
 		go func() {
-			err = cache.HSet(context, "c_sess:"+token, cus_session).Err()
+			_, err := cache.TxPipelined(context, func(pipe redis.Pipeliner) error {
+				pipe.HSet(context, "c_sess:"+token, cus_session)
+				pipe.Expire(context, "c_sess:"+token, 5*time.Minute) // Global expiry on the key.
+				return nil
+			})
+
+			// err = cache.HSet(context, "c_sess:"+token, cus_session).Err()
 			if err != nil {
 				// Log the error but don't fail the request
 				// The user session is still returned from the database
@@ -56,20 +64,26 @@ func GetBackendUserSession(token string, api *types.Api, context context.Context
 
 	if err != nil {
 		if !errors.Is(err, redis.Nil) {
-			return nil, userErrors.NewTokenErr(500, "Something went wrong while getting your session data. Please try again later.")
+			return nil, userErrors.NewTokenErr(fiber.StatusInternalServerError, "Something went wrong while getting your session data. Please try again later.")
 		}
 
 		result := db.WithContext(context).First(backend_session, "token = ?", token)
 		if result.Error != nil {
 			if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-				return nil, userErrors.NewTokenErr(401, "User Session not found. Consider logging in again")
+				return nil, userErrors.NewTokenErr(fiber.StatusUnauthorized, "User Session not found. Consider logging in again")
 			}
 
-			return nil, userErrors.NewTokenErr(500, "Something went wrong while fetching your session data. Please try again later.")
+			return nil, userErrors.NewTokenErr(fiber.StatusInternalServerError, "Something went wrong while fetching your session data. Please try again later.")
 		}
 
 		go func() {
-			err = cache.HSet(context, "b_sess:"+token, backend_session).Err()
+			_, err := cache.TxPipelined(context, func(pipe redis.Pipeliner) error {
+				pipe.HSet(context, "b_sess:"+token, backend_session)
+				pipe.Expire(context, "b_sess:"+token, 5*time.Minute) // Global expiry on the key.
+				return nil
+			})
+
+			// err = cache.HSet(context, "b_sess:"+token, backend_session).Err()
 			if err != nil {
 				// Log the error but don't fail the request
 				// The user session is still returned from the database

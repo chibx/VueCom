03fac409b61d2d80041587a590a9533b8abad14b
chibx <111882496+chibx@users.noreply.github.com>
2025-12-22 21:49:54 +0100

    Auth middleware

--- backend/gateway_service/api/v1/middlewares/auth.go
+++ backend/gateway_service/api/v1/middlewares/auth.go
@@ -2,10 +2,12 @@ package middlewares
 
 import (
 	"errors"
+	"net/url"
 	"strings"
 	"vuecom/gateway/internal/cache"
 	"vuecom/gateway/internal/utils"
 	"vuecom/gateway/internal/v1/types"
+	userErrors "vuecom/shared/errors/users"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -42,22 +44,37 @@ func AuthMiddleware(api *types.Api) fiber.Handler {
 		if auth == "" || !strings.HasPrefix(auth, "Bearer ") {
 			return ctx.Status(fiber.StatusUnauthorized).SendString("Unauthorized")
 		}
-		tokenStr := strings.TrimPrefix(auth, "Bearer ")
+		// tokenStr := strings.TrimPrefix(auth, "Bearer ")
 
 		routeParts := utils.ExtractRouteParts(ctx.Path())
 
-		if len(routeParts) > 1 {
-			if routeParts[1] == api.AdminSlug {
-				backend_token := ctx.Cookies("backend_session")
-				if strings.TrimSpace(backend_token) == "" {
-					return ctx.Redirect(routeParts[1] + "/login") //
-				}
+		// Validate the user if he is accessing the admin panel
+		if len(routeParts) > 1 && routeParts[1] == api.AdminSlug {
+			backend_token := ctx.Cookies("backend_session")
 
-				_, err := cache.GetBackendUserSession(backend_token, api, ctx.Context())
-				if errors.As() {
-					return ctx.Status().Redirect(routeParts[1] + "/login")
-				}
+			if len(routeParts) > 2 && routeParts[2] == "login" {
+				return ctx.Next() // Skip auth for login page
 			}
+
+			if strings.TrimSpace(backend_token) == "" {
+				return ctx.Redirect(routeParts[1] + "/login") //
+			}
+
+			var tokenErr *userErrors.TokenErr
+			backendUserData, err := cache.GetBackendUserSession(backend_token, api, ctx.Context())
+			if errors.As(err, &tokenErr) {
+				if tokenErr.Code == fiber.StatusUnauthorized {
+					absoluteUrl := utils.GetAbsoluteUrl(ctx)
+
+					return ctx.Redirect(routeParts[1]+"/login?redirectTo="+url.QueryEscape(absoluteUrl), fiber.StatusSeeOther)
+				}
+				// Handle other token errors if needed
+				return ctx.Status(tokenErr.Code).SendString(tokenErr.Message)
+			}
+
+			// Store backend user data in context for downstream handlers
+			ctx.Locals("backend_user", backendUserData)
+			return ctx.Next()
 		}
 
 		return ctx.Next()

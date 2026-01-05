0dcff251556fc971a6835bc796f7e43dc5842501
chibx <111882496+chibx@users.noreply.github.com>
2026-01-06 00:06:16 +0100

    Added proper logging

--- backend/services/gateway_service/internal/cache/auth.go
+++ backend/services/gateway_service/internal/cache/auth.go
@@ -12,11 +12,13 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/redis/go-redis/v9"
+	"go.uber.org/zap"
 )
 
 func GetCustomerSession(token string, api *types.Api, context context.Context) (*dbModels.CustomerSession, error) {
 	db := api.Deps.DB
 	cache := api.Deps.Redis
+	logger := api.Deps.Logger
 
 	cus_session := &dbModels.CustomerSession{}
 
@@ -24,32 +26,32 @@ func GetCustomerSession(token string, api *types.Api, context context.Context) (
 
 	if err != nil {
 		if !errors.Is(err, redis.Nil) {
-			return nil, serverErrors.NewTokenErr(fiber.StatusInternalServerError, "Something went wrong while getting your session data. Please try again later.")
+			logger.Error("failed to get customer session from cache", zap.Error(err))
+			return nil, serverErrors.NewServerErr(fiber.StatusInternalServerError, "Something went wrong while getting your session data. Please try again later.")
 		}
 
-		// result := db.WithContext(context).First(cus_session, "token = ?", token)
-
+		logger.Info("customer session not found in cache, fetching from db")
 		cus_session, err = db.Customers().GetSessionByToken(token, context)
 		if err != nil {
 			if errors.Is(err, types.ErrDbNil) {
-				return nil, serverErrors.NewTokenErr(fiber.StatusUnauthorized, "User Session not found. Consider logging in again")
+				logger.Error("customer session not found in db", zap.Error(err))
+				return nil, serverErrors.NewServerErr(fiber.StatusUnauthorized, "User Session not found. Consider logging in again")
 			}
 
-			return nil, serverErrors.NewTokenErr(fiber.StatusInternalServerError, "Something went wrong while fetching your session data. Please try again later.")
+			logger.Error("failed to get customer session from db", zap.Error(err))
+			return nil, serverErrors.NewServerErr(fiber.StatusInternalServerError, "Something went wrong while fetching your session data. Please try again later.")
 		}
 
 		go func() {
+			logger.Info("caching customer session")
 			_, err := cache.TxPipelined(context, func(pipe redis.Pipeliner) error {
 				pipe.HSet(context, constants.CUST_SESS+token, cus_session)
 				pipe.Expire(context, constants.CUST_SESS+token, 5*time.Minute) // Global expiry on the key.
 				return nil
 			})
 
-			// err = cache.HSet(context, constants.C_SESS+token, cus_session).Err()
 			if err != nil {
-				// Log the error but don't fail the request
-				// The user session is still returned from the database
-				// TODO: Add logging here
+				logger.Error("failed to cache customer session", zap.Error(err))
 			}
 		}()
 	}
@@ -60,6 +62,7 @@ func GetCustomerSession(token string, api *types.Api, context context.Context) (
 func GetBackendUserSession(token string, api *types.Api, context context.Context) (*dbModels.BackendSession, error) {
 	db := api.Deps.DB
 	cache := api.Deps.Redis
+	logger := api.Deps.Logger
 
 	backend_session := &dbModels.BackendSession{}
 
@@ -67,33 +70,33 @@ func GetBackendUserSession(token string, api *types.Api, context context.Context
 
 	if err != nil {
 		if !errors.Is(err, redis.Nil) {
-			return nil, serverErrors.NewTokenErr(fiber.StatusInternalServerError, "Something went wrong while getting your session data. Please try again later.")
+			logger.Error("failed to get backend user session from cache", zap.Error(err))
+			return nil, serverErrors.NewServerErr(fiber.StatusInternalServerError, "Something went wrong while getting your session data. Please try again later.")
 		}
 
-		// result := db.WithContext(context).First(backend_session, "token = ?", token)
-
+		logger.Info("backend user session not found in cache, fetching from db")
 		backend_session, err = db.BackendUsers().GetSessionByToken(token, context)
 
 		if err != nil {
 			if errors.Is(err, types.ErrDbNil) {
-				return nil, serverErrors.NewTokenErr(fiber.StatusUnauthorized, "User Session not found. Consider logging in again")
+				logger.Error("backend user session not found in db", zap.Error(err))
+				return nil, serverErrors.NewServerErr(fiber.StatusUnauthorized, "User Session not found. Consider logging in again")
 			}
 
-			return nil, serverErrors.NewTokenErr(fiber.StatusInternalServerError, "Something went wrong while fetching your session data. Please try again later.")
+			logger.Error("failed to get backend user session from db", zap.Error(err))
+			return nil, serverErrors.NewServerErr(fiber.StatusInternalServerError, "Something went wrong while fetching your session data. Please try again later.")
 		}
 
 		go func() {
+			logger.Info("caching backend user session")
 			_, err := cache.TxPipelined(context, func(pipe redis.Pipeliner) error {
 				pipe.HSet(context, constants.BU_SESS+token, backend_session)
 				pipe.Expire(context, constants.BU_SESS+token, 5*time.Minute) // Global expiry on the key.
 				return nil
 			})
 
-			// err = cache.HSet(context, constants.BU_SESS+token, backend_session).Err()
 			if err != nil {
-				// Log the error but don't fail the request
-				// The user session is still returned from the database
-				// TODO: Add logging here
+				logger.Error("failed to cache backend user session", zap.Error(err))
 			}
 		}()
 	}
@@ -104,38 +107,40 @@ func GetBackendUserSession(token string, api *types.Api, context context.Context
 func GetBackendUserById(api *types.Api, id int, context context.Context) (*dbModels.BackendUser, error) {
 	db := api.Deps.DB
 	cache := api.Deps.Redis
+	logger := api.Deps.Logger
 	backendUser := &dbModels.BackendUser{}
 
 	// Try to get from cache first
 	err := cache.HGetAll(context, constants.BU_KEY+strconv.Itoa(id)).Scan(backendUser)
 	if err != nil {
 		if !errors.Is(err, redis.Nil) {
-			return nil, serverErrors.NewTokenErr(fiber.StatusInternalServerError, "Something went wrong while getting your session data. Please try again later.")
+			logger.Error("failed to get backend user from cache", zap.Error(err))
+			return nil, serverErrors.NewServerErr(fiber.StatusInternalServerError, "Something went wrong while getting your session data. Please try again later.")
 		}
 
-		// result := db.WithContext(context).First(backendUser, "id = ?", id)
+		logger.Info("backend user not found in cache, fetching from db")
 		backendUser, err = db.BackendUsers().GetUserById(id, context)
 
 		if err != nil {
 			if errors.Is(err, types.ErrDbNil) {
-				return nil, serverErrors.NewTokenErr(fiber.StatusUnauthorized, "User not found. Consider logging in again")
+				logger.Error("backend user not found in db", zap.Error(err))
+				return nil, serverErrors.NewServerErr(fiber.StatusUnauthorized, "User not found. Consider logging in again")
 			}
 
-			return nil, serverErrors.NewTokenErr(fiber.StatusInternalServerError, "Something went wrong while fetching your session data. Please try again later.")
+			logger.Error("failed to get backend user from db", zap.Error(err))
+			return nil, serverErrors.NewServerErr(fiber.StatusInternalServerError, "Something went wrong while fetching your session data. Please try again later.")
 		}
 
 		go func() {
+			logger.Info("caching backend user")
 			_, err := cache.TxPipelined(context, func(pipe redis.Pipeliner) error {
 				pipe.HSet(context, constants.BU_KEY+strconv.Itoa(id), backendUser)
 				pipe.Expire(context, constants.BU_KEY+strconv.Itoa(id), 5*time.Minute) // Global expiry on the key.
 				return nil
 			})
 
-			// err = cache.HSet(context, constants.BU_KEY+strconv.Itoa(id), backendUser).Err()
 			if err != nil {
-				// Log the error but don't fail the request
-				// The user session is still returned from the database
-				// TODO: Add logging here
+				logger.Error("failed to cache backend user", zap.Error(err))
 			}
 		}()
 	}
@@ -146,37 +151,40 @@ func GetBackendUserById(api *types.Api, id int, context context.Context) (*dbMod
 func GetCustomerById(api *types.Api, id int, context context.Context) (*dbModels.Customer, error) {
 	db := api.Deps.DB
 	cache := api.Deps.Redis
+	logger := api.Deps.Logger
 	customer := &dbModels.Customer{}
 
 	// Try to get from cache first
 	err := cache.HGetAll(context, constants.CUST_KEY+strconv.Itoa(id)).Scan(customer)
 	if err != nil {
 		if !errors.Is(err, redis.Nil) {
-			return nil, serverErrors.NewTokenErr(fiber.StatusInternalServerError, "Something went wrong while getting your session data. Please try again later.")
+			logger.Error("failed to get customer from cache", zap.Error(err))
+			return nil, serverErrors.NewServerErr(fiber.StatusInternalServerError, "Something went wrong while getting your session data. Please try again later.")
 		}
 
+		logger.Info("customer not found in cache, fetching from db")
 		customer, err = db.Customers().GetUserById(id, context)
 
 		if err != nil {
 			if errors.Is(err, types.ErrDbNil) {
-				return nil, serverErrors.NewTokenErr(fiber.StatusUnauthorized, "Customer not found. Consider logging in again")
+				logger.Error("customer not found in db", zap.Error(err))
+				return nil, serverErrors.NewServerErr(fiber.StatusUnauthorized, "Customer not found. Consider logging in again")
 			}
 
-			return nil, serverErrors.NewTokenErr(fiber.StatusInternalServerError, "Something went wrong while fetching your session data. Please try again later.")
+			logger.Error("failed to get customer from db", zap.Error(err))
+			return nil, serverErrors.NewServerErr(fiber.StatusInternalServerError, "Something went wrong while fetching your session data. Please try again later.")
 		}
 
 		go func() {
+			logger.Info("caching customer")
 			_, err := cache.TxPipelined(context, func(pipe redis.Pipeliner) error {
 				pipe.HSet(context, constants.CUST_KEY+strconv.Itoa(id), customer)
 				pipe.Expire(context, constants.CUST_KEY+strconv.Itoa(id), 5*time.Minute) // Global expiry on the key.
 				return nil
 			})
 
-			// err = cache.HSet(context, constants.CUST_KEY+strconv.Itoa(id), customer).Err()
 			if err != nil {
-				// Log the error but don't fail the request
-				// The user session is still returned from the database
-				// TODO: Add logging here
+				logger.Error("failed to cache customer", zap.Error(err))
 			}
 		}()
 	}

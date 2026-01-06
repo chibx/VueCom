3858be7e0fd10b9b1ce67e57f438c83ea29a55c7
chibx <111882496+chibx@users.noreply.github.com>
2026-01-06 23:22:15 +0100

    New Commit

--- backend/services/gateway_service/internal/constants/constants.go
+++ backend/services/gateway_service/internal/constants/constants.go
@@ -3,6 +3,8 @@ package constants
 import (
 	"os"
 	"time"
+
+	"github.com/go-redis/redis_rate/v10"
 )
 
 // Simple way of doing this, a better way would be to use a manifest from a bundler
@@ -19,6 +21,27 @@ var PublicFolder = func() string {
 	}
 	return "dist"
 }()
+
+var (
+	// Token bucket configurations
+	GlobalLimit = redis_rate.Limit{
+		Rate:   50000, // Total requests across the entire app
+		Period: time.Minute,
+		Burst:  100000, // Allow large bursts (e.g., traffic spikes)
+	}
+
+	CustomerLimit = redis_rate.Limit{
+		Rate:   100, // Per customer
+		Period: time.Minute,
+		Burst:  200,
+	}
+
+	BackendLimit = redis_rate.Limit{
+		Rate:   1000, // Higher for devs/admins
+		Period: time.Minute,
+		Burst:  2000,
+	}
+)
 
 const BACKEND_SESSION_TIMEOUT = 30 * time.Minute
 const ApiKeyCtxKey = "api_key"

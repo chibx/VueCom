bd55b2de9933e1a86634bf09e9346039886cf03d
chibx <111882496+chibx@users.noreply.github.com>
2026-01-07 02:52:09 +0100

    New Commit

--- backend/services/gateway_service/api/v1/middlewares/serve-index.go
+++ backend/services/gateway_service/api/v1/middlewares/serve-index.go
@@ -12,37 +12,79 @@ import (
 	dbModels "vuecom/shared/models/db"
 
 	"github.com/gofiber/fiber/v2"
-	"go.uber.org/zap"
 )
+
+// func ServeIndex(api *types.Api) fiber.Handler {
+// 	return func(ctx *fiber.Ctx) error {
+// 		logger := api.Deps.Logger
+// 		routeParts := utils.ExtractRouteParts(ctx.Path())
+// 		var backendToken = strings.TrimSpace(ctx.Cookies(constants.BackendCookieKey))
+// 		var backendUser, _ = ctx.Locals(constants.BackendUserCtxKey).(*dbModels.BackendUser)
+
+// 		// Validate the user if he is accessing the admin panel
+// 		if len(routeParts) > 1 && routeParts[1] == api.AdminSlug {
+
+// 			if len(routeParts) > 2 && routeParts[2] == "login" {
+// 				if backendUser != nil {
+// 					return ctx.Redirect("/" + routeParts[1] + "/dashboard")
+// 				}
+// 				return utils.ServeIndex(ctx)
+// 			}
+
+// 			absoluteUrl := utils.GetAbsoluteUrl(ctx)
+// 			if backendToken == "" {
+// 				logger.Info("Redirecting to login", zap.String("route", routeParts[1]))
+// 				return ctx.Redirect("/" + routeParts[1] + "/login")
+// 			}
+
+// 			if backendUser == nil {
+// 				return ctx.Redirect("/"+routeParts[1]+"/login?redirectTo="+url.QueryEscape(absoluteUrl), fiber.StatusSeeOther)
+// 			}
+
+// 		}
+
+// 		return ctx.Next()
+// 	}
+// }
 
 func ServeIndex(api *types.Api) fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
 		logger := api.Deps.Logger
+		absoluteUrl := utils.GetAbsoluteUrl(ctx)
 		routeParts := utils.ExtractRouteParts(ctx.Path())
 		var backendToken = strings.TrimSpace(ctx.Cookies(constants.BackendCookieKey))
 		var backendUser, _ = ctx.Locals(constants.BackendUserCtxKey).(*dbModels.BackendUser)
-
+		var isLoginRoute = len(routeParts) == 2 && routeParts[1] == "login"
 		// Validate the user if he is accessing the admin panel
-		if len(routeParts) > 1 && routeParts[1] == api.AdminSlug {
+		// if len(routeParts) > 1 && routeParts[1] == api.AdminSlug {
 
-			if len(routeParts) > 2 && routeParts[2] == "login" {
-				if backendUser != nil {
-					return ctx.Redirect("/" + routeParts[1] + "/dashboard")
-				}
-				return utils.ServeIndex(ctx)
+		if isLoginRoute {
+			if backendUser != nil {
+				return ctx.Redirect("/dashboard")
 			}
+			// return utils.ServeIndex(ctx)
+			return ctx.Next()
+		}
 
+		var redirectTo string
+		if isLoginRoute {
+			redirectTo = "?redirectTo=" + url.QueryEscape(absoluteUrl)
+		}
+
+		// prevent redirect on api route
+		if len(routeParts) == 0 || (len(routeParts) > 1 && routeParts[1] != "api") {
 			if backendToken == "" {
-				logger.Info("Redirecting to login", zap.String("route", routeParts[1]))
-				return ctx.Redirect("/" + routeParts[1] + "/login")
+				logger.Info("Redirecting to login")
+				return ctx.Redirect("/login" + redirectTo)
 			}
 
 			if backendUser == nil {
-				absoluteUrl := utils.GetAbsoluteUrl(ctx)
-				return ctx.Redirect("/"+routeParts[1]+"/login?redirectTo="+url.QueryEscape(absoluteUrl), fiber.StatusSeeOther)
+				logger.Info("Redirecting to login")
+				return ctx.Redirect("/login"+redirectTo, fiber.StatusSeeOther)
 			}
-
 		}
+
+		// }
 
 		return ctx.Next()
 	}

74088db4c3adcdcfa484c468ae7f3f92cb538897
chibx <111882496+chibx@users.noreply.github.com>
2026-01-14 07:51:04 +0100

    New COmmit

--- deploy-config/sql/00-init.sql
+++ deploy-config/sql/00-init.sql
@@ -1,10 +1,14 @@
-CREATE DATABASE catalog;
-CREATE DATABASE orders;
-CREATE DATABASE users;
-CREATE DATABASE inventory;
+CREATE DATABASE vuecom_catalog;
+CREATE DATABASE vuecom_orders;
+CREATE DATABASE vuecom_users;
+CREATE DATABASE vuecom_inventory;
+CREATE DATABASE vuecom_payments;
+CREATE DATABASE vuecom_notifications;
 
 CREATE USER vuecom;
-GRANT ALL PRIVILEGES ON DATABASE catalog TO vuecom;
-GRANT ALL PRIVILEGES ON DATABASE orders TO vuecom;
-GRANT ALL PRIVILEGES ON DATABASE users TO vuecom;
-GRANT ALL PRIVILEGES ON DATABASE inventory TO vuecom;
+GRANT ALL PRIVILEGES ON DATABASE vuecom_catalog TO vuecom;
+GRANT ALL PRIVILEGES ON DATABASE vuecom_orders TO vuecom;
+GRANT ALL PRIVILEGES ON DATABASE vuecom_users TO vuecom;
+GRANT ALL PRIVILEGES ON DATABASE vuecom_inventory TO vuecom;
+GRANT ALL PRIVILEGES ON DATABASE vuecom_payments TO vuecom;
+GRANT ALL PRIVILEGES ON DATABASE vuecom_notifications TO vuecom;

3207f0e198f408b793046220b6e79dd1408cfd6e
chibx <111882496+chibx@users.noreply.github.com>
2026-01-18 01:26:28 +0100

    Finally docker is working now

--- deploy-config/sql/00-init.sql
+++ deploy-config/sql/00-init.sql
@@ -6,10 +6,3 @@ CREATE DATABASE vuecom_users;
 CREATE DATABASE vuecom_inventory;
 CREATE DATABASE vuecom_payments;
 CREATE DATABASE vuecom_notifications;
-
-GRANT ALL PRIVILEGES ON DATABASE vuecom_catalog TO vuecom;
-GRANT ALL PRIVILEGES ON DATABASE vuecom_orders TO vuecom;
-GRANT ALL PRIVILEGES ON DATABASE vuecom_users TO vuecom;
-GRANT ALL PRIVILEGES ON DATABASE vuecom_inventory TO vuecom;
-GRANT ALL PRIVILEGES ON DATABASE vuecom_payments TO vuecom;
-GRANT ALL PRIVILEGES ON DATABASE vuecom_notifications TO vuecom;

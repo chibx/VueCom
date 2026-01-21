ca25a49d8309b88648f693664862930f633a166d
chibx <111882496+chibx@users.noreply.github.com>
2026-01-21 16:41:51 +0100

    Added new Constants

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,1 +1,7 @@
+import { $fetch } from 'ofetch'
+
 export const MAX_PASSWORD_LEN = 30
+export const MAX_USERNAME_LEN = 30
+export const backendFetch = $fetch.create({
+    baseURL: '/api/backend',
+})

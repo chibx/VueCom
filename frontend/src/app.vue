2b50cf332e8e9a7bf0408dee26c47dcb27deef53
chibx <111882496+chibx@users.noreply.github.com>
2026-01-21 01:43:21 +0100

    Added Dark theme support and sonner Toaster

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,5 +1,21 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { Toaster } from '@/components/ui/sonner'
+import { useDark } from '@vueuse/core'
+
+const dark = useDark()
+</script>
 
 <template>
-  <RouterView />
+  <div class="h-full bg-slate-50 dark:bg-gray-950">
+    <RouterView />
+  </div>
+  <Toaster />
 </template>
+
+<style>
+html,
+body,
+#app {
+  height: 100%;
+}
+</style>

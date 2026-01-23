f816803547d85b8a1db9bc5281f6ee3d98c940ca
chibx <111882496+chibx@users.noreply.github.com>
2026-01-23 04:30:36 +0100

    Made some refinement to the users db's customer schema and models

--- backend/shared/models/db/users/customers.go
+++ backend/shared/models/db/users/customers.go
@@ -14,24 +14,39 @@ type CustomerAddress struct {
 	ID            uint     `gorm:"primarykey" redis:"id"`
 	CustomerID    uint     `gorm:"not null;index" redis:"user_id"`
 	StreetAddress string   `gorm:"index;not null" redis:"address"`
-	City          string   `gorm:"index;not null" redis:"city"`
-	ZipCode       string   `gorm:"index;not null" redis:"zip_code"`
-	StateID       *uint    `gorm:"column:state;index" redis:"state"`
-	CountryId     *uint    `gorm:"column:country;index" redis:"country"`
+	ZipCode       *string  `gorm:"" redis:"zip_code"`
+	StateID       *uint    `gorm:"" redis:"state"`
+	CountryId     *uint    `gorm:"" redis:"country"`
 	State         *State   `gorm:"foreignKey:StateID;" redis:"-"`
 	Country       *Country `gorm:"foreignKey:CountryId;" redis:"-"`
+	// City          string   `gorm:"index;not null" redis:"city"`
 }
 
 type Customer struct {
-	ID              uint              `gorm:"primarykey" redis:"id"`
-	CreatedAt       time.Time         `gorm:"" redis:"created_at"`
-	FullName        string            `gorm:"not null;type:varchar(255);index" redis:"full_name"`
-	UserName        *string           `gorm:"type:varchar(255);index" redis:"user_name"`
-	PasswordHash    *string           `gorm:"" redis:"-"`
-	Email           string            `gorm:"unique;not null;type:varchar(255);index" redis:"email"`
-	IsEmailVerified bool              `gorm:"default:FALSE;not null" redis:"is_email_verified"`
-	PhoneNumber     *string           `gorm:"type:varchar(20);" redis:"phone_number"`
-	Image           *string           `gorm:"column:image_url" redis:"image_url"`
+	ID              uint      `gorm:"primarykey" redis:"id"`
+	CreatedAt       time.Time `gorm:"" redis:"created_at"`
+	FullName        string    `gorm:"not null;type:varchar(255);index" redis:"full_name"`
+	PasswordHash    *string   `gorm:"" redis:"-"`
+	Email           string    `gorm:"unique;not null;type:varchar(255);index" redis:"email"`
+	IsEmailVerified bool      `gorm:"default:FALSE;not null" redis:"is_email_verified"`
+	PhoneNumber     *string   `gorm:"type:varchar(20);" redis:"phone_number"`
+	ImageUrl        *string   `gorm:"column:image_url" redis:"image_url"`
+	CountryID       *uint     ``
+
+	/*  id SERIAL PRIMARY KEY,
+	    full_name VARCHAR(255) NOT NULL,
+	    email VARCHAR(100) NOT NULL UNIQUE,
+	    phone_number VARCHAR(20) NOT NULL,
+	    address VARCHAR(255) NOT NULL,
+	    city VARCHAR(50) NOT NULL,
+	    state VARCHAR(50) NOT NULL,
+	    zip_code VARCHAR(20) NOT NULL,
+	    country INT,
+	    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	    is_email_verified BOOLEAN DEFAULT FALSE,
+	    image_url TEXT,
+	    password_hash TEXT, */
+
 	BillingAddress  *CustomerAddress  `gorm:"foreignKey:CustomerID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" redis:"-"`
 	ShippingAddress *CustomerAddress  `gorm:"foreignKey:CustomerID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" redis:"-"`
 	Sessions        []CustomerSession `gorm:"foreignKey:CustomerID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" redis:"-"`
--- deploy-config/sql/04-users-01.sql
+++ deploy-config/sql/04-users-01.sql
@@ -4,19 +4,30 @@ CREATE TABLE customers (
     id SERIAL PRIMARY KEY,
     full_name VARCHAR(255) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    phone VARCHAR(20) NOT NULL,
-    address VARCHAR(255) NOT NULL,
-    city VARCHAR(50) NOT NULL,
-    state VARCHAR(50) NOT NULL,
-    zip_code VARCHAR(20) NOT NULL,
-    country INT,
+    phone_number VARCHAR(20) NOT NULL,
+    country_id INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     is_email_verified BOOLEAN DEFAULT FALSE,
     image_url TEXT,
-    user_name VARCHAR(50) UNIQUE,
     password_hash TEXT,
     FOREIGN KEY (country) REFERENCES countries(id)
 );
+
+CREATE INDEX IF NOT EXISTS customers_email_idx ON customers USING hash (email);
+
+CREATE TABLE customer_addresses (
+    id SERIAL PRIMARY KEY,
+    customer_id INT NOT NULL,
+    street_address TEXT NOT NULL,
+    zip_code TEXT,
+    state_id INT,
+    country_id INT,
+    FOREIGN KEY (country_id) REFERENCES countries(id)
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
+
+CREATE INDEX IF NOT EXISTS customer_address_id_idx ON customer_addresses USING hash (customer_id);
+
 
 CREATE TABLE customer_sessions (
     id SERIAL PRIMARY KEY,
@@ -29,12 +40,18 @@ CREATE TABLE customer_sessions (
     FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE CASCADE
 );
 
+CREATE INDEX IF NOT EXISTS customer_sessions_id_idx ON customer_sessions USING hash (customer_id);
+CREATE INDEX IF NOT EXISTS customer_sessions_expires_at_idx ON customer_sessions (expires_at);
+
 CREATE TABLE customer_otps (
     customer_id INT NOT NULL,
     code VARCHAR(10) NOT NULL,
     expiry_date TIMESTAMP NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE CASCADE
+    FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE CASCADE,
+    UNIQUE(customer_id, code)
 );
+
+CREATE INDEX IF NOT EXISTS customer_otps_expiry_idx ON customer_otps (expiry_date);
 
 CREATE TABLE customer_wishlists (
     customer_id INT NOT NULL,
@@ -44,11 +61,18 @@ CREATE TABLE customer_wishlists (
     UNIQUE (customer_id, product_id)
 );
 
+CREATE INDEX IF NOT EXISTS customer_wishlists_id_idx ON customer_wishlists (customer_id);
+CREATE INDEX IF NOT EXISTS customer_wishlists_product_idx ON customer_wishlists (product_id);
+
 CREATE TABLE customer_carts (
     customer_id INT NOT NULL,
+    -- indirect
     product_id BIGINT NOT NULL,
     quantity INT DEFAULT 1,
     added_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE CASCADE,
     UNIQUE (customer_id, product_id)
 );
+
+CREATE INDEX IF NOT EXISTS customer_cart_id_idx ON customer_cart (customer_id);
+CREATE INDEX IF NOT EXISTS customer_cart_product_idx ON customer_cart (product_id);

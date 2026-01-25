1910d20cd80cc4f1f7a00377fa3feb4e7fb6198a
chibx <111882496+chibx@users.noreply.github.com>
2026-01-25 01:47:49 +0100

    Switching to JWT
    Big fault on my part

--- deploy-config/sql/04-users-00.sql
+++ deploy-config/sql/04-users-00.sql
@@ -14,7 +14,7 @@ CREATE TABLE countries (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     -- e.g., 'US', 'NG'
-    code TEXT NOT NULL UNIQUE,
+    code TEXT NOT NULL UNIQUE
 );
 
 CREATE INDEX IF NOT EXISTS idx_countries_code ON countries (code);
@@ -37,10 +37,10 @@ CREATE TABLE backend_users (
     phone_number VARCHAR(20),
     role TEXT DEFAULT 'staff', -- Set to allow custom roles
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     image TEXT,
     country_id INT,
-    is_email_verified BOOLEAN DEFAULT FALSE, 
+    is_email_verified BOOLEAN DEFAULT FALSE,
     FOREIGN KEY (country_id) REFERENCES countries(id)
 );
 
@@ -48,19 +48,36 @@ CREATE INDEX IF NOT EXISTS backend_user_email_idx ON backend_users USING hash (e
 CREATE INDEX IF NOT EXISTS backend_user_email_idx ON backend_users USING hash (username);
 CREATE INDEX IF NOT EXISTS backend_user_role_idx ON backend_users(role);
 
+-- CREATE TABLE backend_sessions (
+--     id SERIAL PRIMARY KEY,
+--     user_id INT NOT NULL,
+--     token TEXT NOT NULL,
+--     ip_address VARCHAR(45),
+--     user_agent TEXT,
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--     expires_at TIMESTAMP NOT NULL,
+--     FOREIGN KEY (user_id) REFERENCES backend_users(id) ON DELETE CASCADE
+-- );
+
+-- CREATE INDEX IF NOT EXISTS backend_sessions_userid_idx ON backend_sessions (user_id);
+-- CREATE INDEX IF NOT EXISTS backend_sessions_expires_at_idx ON backend_sessions (expires_at);
+
 CREATE TABLE backend_sessions (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id INT NOT NULL,
-    token TEXT NOT NULL,
-    ip_address VARCHAR(45),
+    refresh_token_hash TEXT NOT NULL,
+    device_id UUID NOT NULL,
+    last_ip TEXT NOT NULL,
     user_agent TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     expires_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES backend_users(id) ON DELETE CASCADE
 );
 
-CREATE INDEX IF NOT EXISTS backend_sessions_userid_idx ON backend_sessions (user_id);
-CREATE INDEX IF NOT EXISTS backend_sessions_expires_at_idx ON backend_sessions (expires_at);
+-- CREATE INDEX idx_backend_sessions_refresh_token ON backend_sessions(refresh_token_hash);
+CREATE INDEX IF NOT EXISTS idx_backend_sessions_user_id ON backend_sessions(user_id);
+CREATE INDEX IF NOT EXISTS idx_backend_sessions_device ON backend_sessions(device_id);
+
 
 CREATE TABLE backend_otps (
     user_id INT NOT NULL,
@@ -82,7 +99,7 @@ CREATE TABLE backend_user_activities (
 );
 
 CREATE INDEX IF NOT EXISTS backend_user_activities_idx ON backend_otps (user_id);
-CREATE INDEX IF NOT EXISTS backend_user_activities_title_idx ON backend_otps hash (log_title);
+CREATE INDEX IF NOT EXISTS backend_user_activities_title_idx ON backend_otps USING hash(log_title);
 
 CREATE TABLE api_keys (
     user_id INT NOT NULL,
@@ -98,7 +115,7 @@ CREATE INDEX IF NOT EXISTS api_keys_prefix_idx ON api_keys (key_prefix);
 CREATE TABLE password_reset_requests (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
-    reset_token VARCHAR(255) NOT NULL UNIQUE,
+    reset_token TEXT NOT NULL UNIQUE,
     requested_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     expires_at TIMESTAMP NOT NULL,
     used BOOLEAN DEFAULT FALSE,
--- deploy-config/sql/04-users-01.sql
+++ deploy-config/sql/04-users-01.sql
@@ -22,26 +22,44 @@ CREATE TABLE customer_addresses (
     zip_code TEXT,
     state_id INT,
     country_id INT,
-    FOREIGN KEY (country_id) REFERENCES countries(id)
+    FOREIGN KEY (country_id) REFERENCES countries(id),
     FOREIGN KEY (state_id) REFERENCES states(id)
 );
 
 CREATE INDEX IF NOT EXISTS customer_address_id_idx ON customer_addresses USING hash (customer_id);
 
 
+-- CREATE TABLE customer_sessions (
+--     id SERIAL PRIMARY KEY,
+--     customer_id INT NOT NULL,
+--     token VARCHAR(255) NOT NULL UNIQUE,
+--     ip_address VARCHAR(45),
+--     user_agent TEXT,
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--     expires_at TIMESTAMP NOT NULL,
+--     FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE CASCADE
+-- );
+
+-- CREATE INDEX IF NOT EXISTS customer_sessions_id_idx ON customer_sessions USING hash (customer_id);
+-- CREATE INDEX IF NOT EXISTS customer_sessions_expires_at_idx ON customer_sessions (expires_at);
+
 CREATE TABLE customer_sessions (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     customer_id INT NOT NULL,
-    token VARCHAR(255) NOT NULL UNIQUE,
-    ip_address VARCHAR(45),
+    refresh_token_hash TEXT NOT NULL,
+    device_id UUID NOT NULL,
+    last_ip TEXT NOT NULL,
     user_agent TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     expires_at TIMESTAMP NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE CASCADE
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES customers(id) ON DELETE CASCADE
 );
 
-CREATE INDEX IF NOT EXISTS customer_sessions_id_idx ON customer_sessions USING hash (customer_id);
-CREATE INDEX IF NOT EXISTS customer_sessions_expires_at_idx ON customer_sessions (expires_at);
+-- CREATE INDEX idx_backend_sessions_refresh_token ON backend_sessions(refresh_token_hash);
+CREATE INDEX IF NOT EXISTS idx_customer_sessions_user_id ON customer_sessions(customer_id);
+CREATE INDEX IF NOT EXISTS idx_customer_sessions_device ON customer_sessions(device_id);
+
+
 
 CREATE TABLE customer_otps (
     customer_id INT NOT NULL,

1c3fdea99d6d48c59d3c2fea274d74fe490e2777
chibx <111882496+chibx@users.noreply.github.com>
2026-01-25 01:54:33 +0100

    Commented out functions i aint using

--- backend/services/gateway/internal/auth/session.go
+++ backend/services/gateway/internal/auth/session.go
@@ -1,13 +1,9 @@
 package auth
 
 import (
-	"context"
 	"crypto/rand"
 
-	userModels "github.com/chibx/vuecom/backend/shared/models/db/users"
-
 	"github.com/chibx/vuecom/backend/services/gateway/internal/constants"
-	"github.com/chibx/vuecom/backend/services/gateway/internal/types"
 )
 
 func GenerateSessionToken() (string, error) {
@@ -28,36 +24,36 @@ func GenerateCustomerSessionToken() (string, error) {
 	return constants.CUST_KEY + string(bytes), nil
 }
 
-func DeleteBackendSession(ctx context.Context, api *types.Api, session *userModels.BackendSession) error {
-	// Delete the session from the database and cache
-	db := api.Deps.DB
-	cache := api.Deps.Redis
-	if err := cache.Unlink(ctx, constants.BU_KEY+session.Token).Err(); err != nil {
-		return err
-	}
+// func DeleteBackendSession(ctx context.Context, api *types.Api, session *userModels.BackendSession_Old) error {
+// 	// Delete the session from the database and cache
+// 	db := api.Deps.DB
+// 	cache := api.Deps.Redis
+// 	if err := cache.Unlink(ctx, constants.BU_KEY+session.Token).Err(); err != nil {
+// 		return err
+// 	}
 
-	// err := db.Model(backendSession).Where("token = ?", token).Delete(backendSession).Error
-	err := db.BackendUsers().DeleteSession(session, ctx)
-	if err != nil {
-		return err
-	}
+// 	// err := db.Model(backendSession).Where("token = ?", token).Delete(backendSession).Error
+// 	err := db.BackendUsers().DeleteSession(session, ctx)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
-func DeleteCustomerSession(ctx context.Context, api *types.Api, session *userModels.CustomerSession) error {
-	// Delete the session from the database and cache
-	db := api.Deps.DB
-	cache := api.Deps.Redis
-	if err := cache.Unlink(ctx, constants.CUST_KEY+session.Token).Err(); err != nil {
-		return err
-	}
+// func DeleteCustomerSession(ctx context.Context, api *types.Api, session *userModels.CustomerSession_Old) error {
+// 	// Delete the session from the database and cache
+// 	db := api.Deps.DB
+// 	cache := api.Deps.Redis
+// 	if err := cache.Unlink(ctx, constants.CUST_KEY+session.Token).Err(); err != nil {
+// 		return err
+// 	}
 
-	// err := db.Model(customerSession).Where("token = ?", token).Delete(customerSession).Error
-	err := db.Customers().DeleteSession(ctx, session)
-	if err != nil {
-		return err
-	}
+// 	// err := db.Model(customerSession).Where("token = ?", token).Delete(customerSession).Error
+// 	err := db.Customers().DeleteSession(ctx, session)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	return nil
-}
+// 	return nil
+// }

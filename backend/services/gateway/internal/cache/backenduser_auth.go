3463861fad941b256adff9fdfde4e90959a1060d
chibx <111882496+chibx@users.noreply.github.com>
2026-01-25 01:51:51 +0100

    Realized that the user doesn't need cache, or it doesnt make sense

--- backend/services/gateway/internal/cache/backenduser_auth.go
+++ backend/services/gateway/internal/cache/backenduser_auth.go
@@ -18,53 +18,6 @@ import (
 	"go.uber.org/zap"
 )
 
-func GetBackendUserSession(token string, api *types.Api, ctx context.Context) (*userModels.BackendSession, error) {
-	db := api.Deps.DB
-	cache := api.Deps.Redis
-	logger := api.Deps.Logger
-
-	var backend_session *userModels.BackendSession
-
-	err := cache.HGetAll(ctx, constants.BU_SESS+token).Scan(&backend_session)
-	notExist := backend_session.UserId == 0
-
-	if err != nil || notExist {
-		if notExist {
-			logger.Info("backend user session not found in cache")
-		} else {
-			logger.Error("failed to get backend user session from cache, fetching from db", zap.Error(err))
-		}
-
-		backend_session, err = db.BackendUsers().GetSessionByToken(ctx, token)
-
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			logger.Error("backend user session not found in db", zap.Error(err))
-			return nil, serverErrors.NewServerErr(fiber.StatusUnauthorized, "User Session not found. Consider logging in again")
-		}
-
-		if err != nil {
-			// err can't be ErrRecordNotFound so it's fine here
-			logger.Error("failed to get backend user session from db", zap.Error(err))
-			return nil, serverErrors.NewServerErr(fiber.StatusInternalServerError, "Something went wrong while fetching your session data. Please try again later.")
-		}
-
-		go func() {
-			logger.Info("caching backend user session")
-			_, err := cache.TxPipelined(ctx, func(pipe redis.Pipeliner) error {
-				pipe.HSet(ctx, constants.BU_SESS+token, backend_session)
-				pipe.Expire(ctx, constants.BU_SESS+token, 5*time.Minute) // Global expiry on the key.
-				return nil
-			})
-
-			if err != nil {
-				logger.Error("failed to cache backend user session", zap.Error(err))
-			}
-		}()
-	}
-
-	return backend_session, nil
-}
-
 func GetBackendUserById(api *types.Api, id int, ctx context.Context) (*userModels.BackendUser, error) {
 	db := api.Deps.DB
 	cache := api.Deps.Redis
@@ -110,22 +63,4 @@ func GetBackendUserById(api *types.Api, id int, ctx context.Context) (*userModel
 	}
 
 	return backendUser, nil
-}
-
-func TouchBackendSession(api *types.Api, token string, ctx context.Context) {
-	rdb := api.Deps.Redis
-	logger := api.Deps.Logger
-
-	// ttl := rdb.TTL(ctx, token)
-	// if ttl.Err() != nil {
-	// 	logger.Error("failed to get backend user session ttl", zap.Error(ttl.Err()))
-	// 	return
-	// }
-	// ttlSeconds := int64(ttl.Val() / time.Second)
-	// logger.Info("backend user session ttl", zap.Int64("ttl", ttlSeconds))
-
-	_, err := rdb.Expire(ctx, constants.BU_SESS+token, constants.BackendSessionTimeout).Result()
-	if err != nil {
-		logger.Error("failed to expire backend user session", zap.Error(err))
-	}
 }
--- backend/services/gateway/internal/cache/customer_auth.go
+++ backend/services/gateway/internal/cache/customer_auth.go
@@ -18,53 +18,6 @@ import (
 	"go.uber.org/zap"
 )
 
-func GetCustomerSession(token string, api *types.Api, ctx context.Context) (*userModels.CustomerSession, error) {
-	db := api.Deps.DB
-	cache := api.Deps.Redis
-	logger := api.Deps.Logger
-
-	cus_session := &userModels.CustomerSession{}
-
-	err := cache.HGetAll(ctx, constants.CUST_SESS+token).Scan(cus_session)
-	notExist := cus_session.UserID == 0
-
-	if err != nil || notExist {
-		if notExist {
-			logger.Info("customer session not found in cache, fetching from db")
-			// return nil, serverErrors.NewServerErr(fiber.StatusInternalServerError, "Something went wrong while getting your session data. Please try again later.")
-		} else {
-			logger.Error("failed to get customer session from cache", zap.Error(err))
-		}
-
-		cus_session, err = db.Customers().GetSessionByToken(ctx, token)
-
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			logger.Error("customer session not found in db", zap.Error(err))
-			return nil, serverErrors.NewServerErr(fiber.StatusUnauthorized, "User Session not found. Consider logging in again")
-		}
-
-		if err != nil {
-			logger.Error("failed to get customer session from db", zap.Error(err))
-			return nil, serverErrors.NewServerErr(fiber.StatusInternalServerError, "Something went wrong while fetching your session data. Please try again later.")
-		}
-
-		go func() {
-			logger.Info("caching customer session")
-			_, err := cache.TxPipelined(ctx, func(pipe redis.Pipeliner) error {
-				pipe.HSet(ctx, constants.CUST_SESS+token, cus_session)
-				pipe.Expire(ctx, constants.CUST_SESS+token, 5*time.Minute) // Global expiry on the key.
-				return nil
-			})
-
-			if err != nil {
-				logger.Error("failed to cache customer session", zap.Error(err))
-			}
-		}()
-	}
-
-	return cus_session, nil
-}
-
 func GetCustomerById(api *types.Api, id int, ctx context.Context) (*userModels.Customer, error) {
 	db := api.Deps.DB
 	cache := api.Deps.Redis
@@ -110,22 +63,4 @@ func GetCustomerById(api *types.Api, id int, ctx context.Context) (*userModels.C
 	}
 
 	return customer, nil
-}
-
-func TouchCustomerSession(api *types.Api, token string, ctx context.Context) {
-	rdb := api.Deps.Redis
-	logger := api.Deps.Logger
-
-	// ttl := rdb.TTL(ctx, token)
-	// if ttl.Err() != nil {
-	// 	logger.Error("failed to get backend user session ttl", zap.Error(ttl.Err()))
-	// 	return
-	// }
-	// ttlSeconds := int64(ttl.Val() / time.Second)
-	// logger.Info("backend user session ttl", zap.Int64("ttl", ttlSeconds))
-
-	_, err := rdb.Expire(ctx, constants.CUST_SESS+token, constants.BackendSessionTimeout).Result()
-	if err != nil {
-		logger.Error("failed to expire customer session", zap.Error(err))
-	}
 }

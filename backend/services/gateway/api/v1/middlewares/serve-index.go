eacbb6bddddd7eda279f6a8bd339a6132f0f5d9c
chibx <111882496+chibx@users.noreply.github.com>
2026-01-26 01:12:47 +0100

    New Commit

--- backend/services/gateway/api/v1/middlewares/serve-index.go
+++ backend/services/gateway/api/v1/middlewares/serve-index.go
@@ -57,9 +57,27 @@ func ServeIndex(api *types.Api) fiber.Handler {
 		var backendToken = strings.TrimSpace(ctx.Cookies(constants.BackendCookieKey))
 		var backendUser, _ = ctx.Locals(constants.BackendUserCtxKey).(*userModels.BackendUser)
 		var isLoginRoute = len(routeParts) == 2 && routeParts[1] == "login"
+		var isAppInitRoute = len(routeParts) == 3 && routeParts[1] == "app" && routeParts[2] == "init"
+		var isAdminCreateRoute = len(routeParts) == 3 && routeParts[1] == "app" && routeParts[2] == "create-user"
 		var redirectTo = "?redirectTo=" + url.QueryEscape(absoluteUrl)
 		// Validate the user if he is accessing the admin panel
 		// if len(routeParts) > 1 && routeParts[1] == api.AdminSlug {
+
+		if isAppInitRoute && api.IsAppInit {
+			if backendUser != nil {
+				return ctx.Redirect("/dashboard")
+			} else {
+				return ctx.Redirect("/login")
+			}
+		}
+
+		if isAdminCreateRoute && api.HasAdmin {
+			if backendUser != nil {
+				return ctx.Redirect("/dashboard")
+			} else {
+				return ctx.Redirect("/login")
+			}
+		}
 
 		if isLoginRoute {
 			if backendUser != nil {

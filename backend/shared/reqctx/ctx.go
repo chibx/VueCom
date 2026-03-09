98a9512a0b28181ae7fe907d93daf410f9b0acf9
chibx <111882496+chibx@users.noreply.github.com>
2026-03-09 19:53:15 +0100

    Added ApiKey field to track whether user is requesting through an api key

--- backend/shared/reqctx/ctx.go
+++ backend/shared/reqctx/ctx.go
@@ -1,8 +1,11 @@
 // Shared types for propagated context data
 package ctx
 
+import "github.com/chibx/vuecom/backend/shared/models/db/users"
+
 type BackendUser struct {
-	ID int
+	ID     int
+	ApiKey *users.ApiKey // To know if the user is acting through an api key
 }
 
 type Customer struct {

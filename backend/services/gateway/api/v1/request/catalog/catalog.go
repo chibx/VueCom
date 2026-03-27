5719b8e32400b51980db20c895ed650a2d318ad6
chibx <111882496+chibx@users.noreply.github.com>
2026-03-27 14:16:36 +0100

    Base Create Product request body

--- backend/services/gateway/api/v1/request/catalog/catalog.go
+++ backend/services/gateway/api/v1/request/catalog/catalog.go
@@ -2,34 +2,31 @@ package catalog
 
 import (
 	"time"
-
-	"github.com/chibx/vuecom/backend/shared/models/db/catalog"
 )
 
 type CreateProductReq struct {
-	P             catalog.Product
-	Name          string     `json:"name"`
-	SKU           string     `json:"sku"`
-	BasePrice     float64    `json:"base_price"`
-	SalePrice     float64    `json:"sale_price"`
-	DiscountStart *time.Time `json:"discount_start"`
-	DiscountEnd   *time.Time `json:"discount_end"`
-	Enabled       bool       `json:"enabled"`
-	// Warranty   string    `json:"warranty"`
-	ShortDescription string   `json:"short_description"`
-	FullDescription  string   `json:"full_description"`
-	Quantity         int      `json:"quantity"`
-	Slug             string   `json:"slug"`
-	Country          uint     `json:"country"`
-	Weight           *float64 `json:"weight"`
-	BrandId          int      `json:"brand_id"`
-	Color            uint     `json:"color"`
-	Medias           []uint   `json:"medias"`
-	MetaTitle        *string  `json:"meta_title,omitempty"`
-	MetaDescription  *string  `json:"meta_description,omitempty"`
-	SearchKeywords   *string  `json:"search_keywords"`
-	RelatedProducts  []string `json:"related_products"`
-	UpSellProducts   []string `json:"upsell"`
-	ParentID         *uint    `json:"parent_id"`
-	PresetID         *uint    `json:"preset_id"`
+	Name             string     `json:"name"`
+	SKU              string     `json:"sku"`
+	BasePrice        float64    `json:"base_price"`
+	SalePrice        float64    `json:"sale_price"`
+	DiscountStart    *time.Time `json:"discount_start"`
+	DiscountEnd      *time.Time `json:"discount_end"`
+	Enabled          bool       `json:"enabled"`
+	ShortDescription string     `json:"short_description"`
+	FullDescription  string     `json:"full_description"`
+	Quantity         int        `json:"quantity"`
+	Slug             string     `json:"slug"`
+	Country          uint       `json:"country"`
+	Weight           *float64   `json:"weight"`
+	BrandId          int        `json:"brand_id"`
+	Color            uint       `json:"color"`
+	Medias           []uint     `json:"medias"`
+	MetaTitle        *string    `json:"meta_title,omitempty"`
+	MetaDescription  *string    `json:"meta_description,omitempty"`
+	SearchKeywords   *string    `json:"search_keywords"`
+	RelatedProducts  []string   `json:"related_products"`
+	UpSellProducts   []string   `json:"upsell"`
+	CrossSell        []uint64   `json:"cross_sell"`
+	ParentID         *uint      `json:"parent_id"`
+	PresetID         *uint      `json:"preset_id"`
 }

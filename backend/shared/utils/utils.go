b18475c3efe5c41d48badcac74800ffbfa8b7b7f
chibx <111882496+chibx@users.noreply.github.com>
2026-04-01 11:16:15 +0100

    Shared Utility fuctions

---
+++ backend/shared/utils/utils.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"time"
+
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func NilOrPointer[T any](e *T) *T {
+	if e == nil {
+		return nil
+	}
+
+	return e
+}
+
+func NilTimeToRpc(t *time.Time) *timestamppb.Timestamp {
+	if t == nil {
+		return nil
+	}
+
+	return timestamppb.New(*t)
+}

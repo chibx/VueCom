495253e1d452a92dc8d3e39347d3c274a5a30d39
chibx <111882496+chibx@users.noreply.github.com>
2026-04-01 11:16:00 +0100

    Changed the data type of the new_from and new_to to be nullable time

--- backend/shared/types/request/catalog.go
+++ backend/shared/types/request/catalog.go
@@ -20,26 +20,26 @@ type CreateProductReq struct {
 	InStock          bool           `json:"in_stock" validate:"required"`
 	DiscountStart    *time.Time     `json:"discount_start"`
 	DiscountEnd      *time.Time     `json:"discount_end"`
-	Categories       []int          `json:"categories"`
+	Categories       []int32        `json:"categories"`
 	IsNew            bool           `json:"is_new"`
-	NewFrom          time.Time      `json:"new_from"`
-	NewTo            time.Time      `json:"new_to"`
+	NewFrom          *time.Time     `json:"new_from"`
+	NewTo            *time.Time     `json:"new_to"`
 	Enabled          bool           `json:"enabled"`
 	ShortDescription string         `json:"short_description"`
 	FullDescription  string         `json:"full_description"`
-	Quantity         int            `json:"quantity"`
+	Quantity         int32          `json:"quantity" validate:"gte=0"`
 	Slug             string         `json:"slug" validate:"required"`
-	CountryOfManf    uint           `json:"country_of_manufacture" validate:"required"`
+	CountryOfManf    uint32         `json:"country_of_manufacture" validate:"required"`
 	Weight           *float64       `json:"weight"`
-	BrandId          int            `json:"brand_id"`
-	ColorId          uint           `json:"color_id"`
-	Medias           []uint         `json:"medias"`
+	BrandId          int32          `json:"brand_id"`
+	ColorId          uint32         `json:"color_id"`
+	Medias           []uint32       `json:"medias"`
 	MetaTitle        string         `json:"meta_title,omitempty" validate:"required"`
 	MetaDescription  string         `json:"meta_description,omitempty" validate:"required"`
 	SearchKeywords   *string        `json:"search_keywords"`
-	RelatedProducts  []uint         `json:"related_products"`
-	UpSellProducts   []uint         `json:"upsell"`
-	CrossSell        []uint         `json:"cross_sell"`
-	PresetID         *uint          `json:"preset_id"`
+	RelatedProducts  []uint32       `json:"related_products"`
+	UpSellProducts   []uint32       `json:"upsell"`
+	CrossSell        []uint32       `json:"cross_sell"`
+	PresetID         *uint32        `json:"preset_id"`
 	PresetValues     map[string]any `json:"preset_values"`
 }

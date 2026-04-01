898628bb20ef945351a7aef2083ca77aa289d449
chibx <111882496+chibx@users.noreply.github.com>
2026-04-01 20:35:53 +0100

    Added redis tags to the product struct

--- backend/shared/models/db/catalog/catalog.go
+++ backend/shared/models/db/catalog/catalog.go
@@ -99,32 +99,32 @@ type Product struct {
 	ID               uint       `gorm:"primarykey" redis:"id"`
 	UpdatedAt        time.Time  `gorm:"" redis:"updated_at"`
 	CreatedAt        time.Time  `gorm:"" redis:"created_at"`
-	Name             string     `json:"name" gorm:"not null;index;type:text" redis:"name"`
-	SKU              string     `json:"sku" gorm:"not null;index" redis:"sku"`
-	BasePrice        float64    `json:"base_price" gorm:"not null;type:numeric(15, 2)" redis:"price"`
-	SalePrice        float64    `json:"sale_price" gorm:"not null;type:numeric(15, 2)" redis:"price"`
-	DiscountStart    *time.Time `json:"discount_start"`
-	DiscountEnd      *time.Time `json:"discount_end"`
-	IsNew            bool
-	NewFrom          *time.Time
-	NewTo            *time.Time
-	CountryOfManf    uint     `gorm:"column:country_of_manufacture;"`
-	Enabled          bool     `json:"enabled" gorm:"default:TRUE;not null"`
-	ShortDescription string   `json:"short_description"`
-	FullDescription  string   `json:"full_description"`
-	Slug             string   `json:"slug" redis:"slug"`
-	Weight           *float64 `json:"weight" redis:"weight"`
-	BrandId          int
-	ColorId          int        `gorm:"" redis:"color_id"`
-	MetaTitle        *string    `json:"meta_title,omitempty" redis:"meta_title"`
-	MetaDescription  *string    `json:"meta_description,omitempty" redis:"meta_title"`
-	SearchKeywords   *string    `json:"search_keywords" gorm:"column:search_keywords;" redis:"search_keywords"`
-	ParentID         *uint      `json:"parent_id" redis:"parent_id"`
-	PresetID         *uint      `json:"preset_id" gorm:"index" redis:"preset_id"`
-	Parent           *Product   `json:"-" gorm:"foreignKey:ParentID"`
-	Preset           *Preset    `json:"-" gorm:"foreignKey:PresetID;constraint:OnUpdate:SET NULL,OnDelete:SET NULL;" redis:"-"`
-	Categories       []Category `json:"-" gorm:"many2many:product_category_values;" redis:"-"`
-	Tags             []Tag      `json:"-" gorm:"many2many:product_tags;" redis:"-"`
+	Name             string     `gorm:"not null;index;type:text" redis:"name"`
+	SKU              string     `gorm:"not null;index" redis:"sku"`
+	BasePrice        float64    `redis:"base_price" gorm:"not null;type:numeric(15, 2)"`
+	SalePrice        float64    `redis:"sale_price" gorm:"not null;type:numeric(15, 2)"`
+	DiscountStart    *time.Time `redis:"discount_start"`
+	DiscountEnd      *time.Time `redis:"discount_end"`
+	IsNew            bool       `redis:"is_new"`
+	NewFrom          *time.Time `redis:"new_from"`
+	NewTo            *time.Time `reids:"new_to"`
+	CountryOfManf    uint       `gorm:"column:country_of_manufacture;" redis:"coun_of_manf"`
+	Enabled          bool       `redis:"enabled" gorm:"default:TRUE;not null"`
+	ShortDescription string     `redis:"short_description"`
+	FullDescription  string     `redis:"full_description"`
+	Slug             string     `redis:"slug"`
+	Weight           *float64   `redis:"weight"`
+	BrandId          int        `redis:"brand_id"`
+	ColorId          int        `redis:"color_id"`
+	MetaTitle        *string    `redis:"meta_title"`
+	MetaDescription  *string    `redis:"meta_title"`
+	SearchKeywords   *string    `gorm:"column:search_keywords;" redis:"search_keywords"`
+	ParentID         *uint      `redis:"parent_id"`
+	PresetID         *uint      `gorm:"index" redis:"preset_id"`
+	Parent           *Product   `gorm:"foreignKey:ParentID"`
+	Preset           *Preset    `gorm:"foreignKey:PresetID;constraint:OnUpdate:SET NULL,OnDelete:SET NULL;" redis:"-"`
+	Categories       []Category `gorm:"many2many:product_category_values;" redis:"-"`
+	Tags             []Tag      `gorm:"many2many:product_tags;" redis:"-"`
 	// Categories  []Category `gorm:"many2many:product_category_values;foreignkey:ID;joinforeignKey:ProductId;References:ID;joinReferences:CategoryId;"`
 }
 

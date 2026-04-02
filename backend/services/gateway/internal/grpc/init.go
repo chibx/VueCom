8744861deefca2cd4e600c940bc8bbd619bc5dc6
chibx <111882496+chibx@users.noreply.github.com>
2026-04-02 13:36:33 +0100

    Add cleanup for each service module

--- backend/services/gateway/internal/grpc/init.go
+++ backend/services/gateway/internal/grpc/init.go
@@ -2,7 +2,9 @@ package grpc
 
 import (
 	catalog_service "github.com/chibx/vuecom/backend/services/catalog"
+	inventory_service "github.com/chibx/vuecom/backend/services/inventory"
 	order_service "github.com/chibx/vuecom/backend/services/orders"
+	payment_service "github.com/chibx/vuecom/backend/services/payment"
 	"github.com/chibx/vuecom/backend/shared/proto/go/catalog"
 	"github.com/chibx/vuecom/backend/shared/proto/go/orders"
 )
@@ -29,6 +31,10 @@ func InitClients() func() {
 	CatalogClient = catalog.NewCatalogServiceClient(conn)
 
 	return func() {
+		catalog_service.Destroy()
+		order_service.Destroy()
+		payment_service.Destroy()
+		inventory_service.Destroy()
 		conn.Close()
 		shutdown()
 	}

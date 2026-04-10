f25735c202352a1742b203f751114761e56d1d49
chibx <111882496+chibx@users.noreply.github.com>
2026-04-10 15:02:10 +0100

    Rename some data types into more realistic types

--- backend/shared/models/db/catalog/catalog.go
+++ backend/shared/models/db/catalog/catalog.go
@@ -130,11 +130,11 @@ type Product struct {
 
 type ProductRelation struct {
 	/** From ID */
-	SourceProductID uint64 `gorm:"not null"`
+	SourceProductID uint32 `gorm:"not null"`
 	/** To ID */
-	TargetProductID uint64 `gorm:"not null"`
-	RelationType    string `gorm:"not null"`
-	SortOrder       int32  `gorm:"default:0"`
+	TargetProductID uint32 `gorm:"not null"`
+	RelationType    uint8  `gorm:"not null"`
+	SortOrder       uint16 `gorm:"default:0"`
 }
 
 type ProductMedia struct {

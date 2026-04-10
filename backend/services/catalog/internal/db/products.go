764af88d055525d8a584bbafe29062a3088d9203
chibx <111882496+chibx@users.noreply.github.com>
2026-04-10 11:49:03 +0100

    Add function to insert related fields for the product-category relationship

--- backend/services/catalog/internal/db/products.go
+++ backend/services/catalog/internal/db/products.go
@@ -2,6 +2,9 @@ package db
 
 import (
 	"context"
+	"fmt"
+	"strconv"
+	"strings"
 
 	serverErrors "github.com/chibx/vuecom/backend/shared/errors/server"
 	catModels "github.com/chibx/vuecom/backend/shared/models/db/catalog"
@@ -11,6 +14,29 @@ import (
 
 func (c *CatalogDB) CreateProduct(ctx context.Context, product *catModels.Product) error {
 	return c.db.WithContext(ctx).Create(product).Error
+}
+
+func (c *CatalogDB) CreateProductToCategory(ctx context.Context, productId uint32, categories []uint32) error {
+	// [RAW SQL] To avoid the allocation of an array/slice of catalog.ProductCategoryValues struct
+	// Is safe (supposedly) because I am inserting integers, not strings
+	if len(categories) == 0 {
+		return nil
+	}
+	var sql strings.Builder
+	sql.WriteString(`INSERT INTO product_category_values (product_id, category_id) VALUES `)
+	for idx, id := range categories {
+		fmt.Fprintf(&sql, "(%s, %s)",
+			strconv.FormatInt(int64(productId), 10),
+			strconv.FormatInt(int64(id), 10),
+		)
+
+		if idx < len(categories)-1 {
+			sql.WriteString(",")
+		}
+	}
+	sql.WriteString(";")
+
+	return c.db.WithContext(ctx).Exec(sql.String()).Error
 }
 
 func (c *CatalogDB) GetProductById(ctx context.Context, id int) (*catModels.Product, error) {

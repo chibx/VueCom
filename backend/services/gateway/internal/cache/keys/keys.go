02c431d3a0bd736b13456f02fb201d54741fe8d9
chibx <111882496+chibx@users.noreply.github.com>
2026-04-13 22:53:26 +0100

    Make some changes to the cache keys package

--- backend/services/gateway/internal/cache/keys/keys.go
+++ backend/services/gateway/internal/cache/keys/keys.go
@@ -7,14 +7,14 @@ const USER_KEY_PREFIX = "user:"
 const PRODUCT_KEY_PREFIX = "product:"
 const BACKEND_USER_KEY_PREFIX = "backend_user:"
 
-func UserKey(userId int) string {
-	return USER_KEY_PREFIX + strconv.Itoa(userId)
+func UserKey(userId uint32) string {
+	return USER_KEY_PREFIX + strconv.FormatUint(uint64(userId), 10)
 }
 
-func ProductKey(productId int) string {
-	return PRODUCT_KEY_PREFIX + strconv.Itoa(productId)
+func ProductKey(productId uint32) string {
+	return PRODUCT_KEY_PREFIX + strconv.FormatUint(uint64(productId), 10)
 }
 
-func BackendUserKey(userId int) string {
-	return BACKEND_USER_KEY_PREFIX + strconv.Itoa(userId)
+func BackendUserKey(userId uint32) string {
+	return BACKEND_USER_KEY_PREFIX + strconv.FormatUint(uint64(userId), 10)
 }

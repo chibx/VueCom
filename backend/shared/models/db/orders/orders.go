df00426e6573e250f6c8efce3108903d60d5738c
chibx <111882496+chibx@users.noreply.github.com>
2026-04-14 15:54:17 +0100

    Add some columns to the order and inventory table

--- backend/shared/models/db/orders/orders.go
+++ backend/shared/models/db/orders/orders.go
@@ -6,14 +6,14 @@ type Order struct {
 	ID                uint      `gorm:"primarykey" redis:"id"`
 	CreatedAt         time.Time `gorm:"" redis:"created_at"`
 	UpdatedAt         time.Time `gorm:"" redis:"updated_at"`
-	UserID            uint      `json:"user_id" gorm:"index;not null" redis:"user_id"`
+	UserID            uint32    `json:"user_id" gorm:"index;not null" redis:"user_id"`
 	OrderNumber       string    `json:"order_number" gorm:"index;not null;unique" redis:"order_number"`
 	TotalAmount       float64   `json:"total_amount" gorm:"not null" redis:"total_amount"`
 	Currency          string    `json:"currency" gorm:"default:'NGN';not null" redis:"currency"`
 	Status            string    `json:"status" gorm:"default:'pending';not null" redis:"status"`
-	BillingAddressID  uint      `json:"billing_address_id" gorm:"index;not null" redis:"billing_address_id"`
-	ShippingAddressID uint      `json:"shipping_address_id" gorm:"index;not null" redis:"shipping_address_id"`
-	PaymentID         uint      `json:"payment_id" gorm:"index;not null" redis:"payment_id"`
+	BillingAddressID  uint32    `json:"billing_address_id" gorm:"index;not null" redis:"billing_address_id"`
+	ShippingAddressID uint32    `json:"shipping_address_id" gorm:"index;not null" redis:"shipping_address_id"`
+	PaymentID         uint32    `json:"payment_id" gorm:"index;not null" redis:"payment_id"`
 }
 
 type OrderReturn struct {
@@ -24,9 +24,10 @@ type OrderReturn struct {
 }
 
 type OrderItem struct {
-	ID       uint    `gorm:"primarykey" redis:"id"`
-	OrderID  uint    `json:"order_id" gorm:"index;not null" redis:"order_id"`
-	Name     string  `json:"name" gorm:"not null" redis:"name"`
-	Price    float64 `json:"price" gorm:"not null" redis:"price"`
-	Quantity uint    `json:"quantity" gorm:"not null" redis:"quantity"`
+	ID          uint    `gorm:"primarykey" redis:"id"`
+	OrderID     uint    `json:"order_id" gorm:"index;not null" redis:"order_id"`
+	Name        string  `json:"name" gorm:"not null" redis:"name"`
+	Price       float64 `json:"price" gorm:"not null" redis:"price"`
+	Quantity    uint    `json:"quantity" gorm:"not null" redis:"quantity"`
+	WarehouseId uint32  ``
 }

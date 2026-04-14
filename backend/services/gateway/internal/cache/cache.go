5779ee97fbab601b8a0b92156eaa0f66181fa14c
chibx <111882496+chibx@users.noreply.github.com>
2026-04-14 09:56:49 +0100

    Move the goroutine function into a reusable function

--- backend/services/gateway/internal/cache/cache.go
+++ backend/services/gateway/internal/cache/cache.go
@@ -4,19 +4,20 @@ import (
 	"context"
 	"time"
 
+	"github.com/chibx/vuecom/backend/services/gateway/api/v1/handlers/backend/products"
+	catRes "github.com/chibx/vuecom/backend/services/gateway/api/v1/response/catalog"
 	"github.com/chibx/vuecom/backend/services/gateway/internal/cache/keys"
 	"github.com/chibx/vuecom/backend/services/gateway/internal/global"
 	igrpc "github.com/chibx/vuecom/backend/services/gateway/internal/grpc"
 	"github.com/chibx/vuecom/backend/services/gateway/internal/types"
 	"github.com/chibx/vuecom/backend/shared/errors/server"
-	catModels "github.com/chibx/vuecom/backend/shared/models/db/catalog"
 	"github.com/chibx/vuecom/backend/shared/proto/go/catalog"
 	"github.com/redis/go-redis/v9"
 	"go.uber.org/zap"
 )
 
-func GetProduct(ctx context.Context, api types.Api, productId uint32) (*catModels.Product, error) {
-	var product = new(catModels.Product)
+func GetProduct(ctx context.Context, api *types.Api, productId uint32) (*catRes.GetProductResp, error) {
+	var product = new(catRes.GetProductResp)
 	var err error
 	rds := api.Deps.Redis
 
@@ -27,6 +28,8 @@ func GetProduct(ctx context.Context, api types.Api, productId uint32) (*catModel
 		resp, err := igrpc.CatalogClient.GetProduct(ctx, &catalog.GetProductRequest{
 			Id: uint64(productId),
 		})
+		product, err = products.GetProductFromRpc(resp)
+
 		if err != nil {
 			if err.Error() == server.ErrDBRecordNotFound.Error() {
 				return nil, server.ErrDBRecordNotFound
@@ -36,21 +39,25 @@ func GetProduct(ctx context.Context, api types.Api, productId uint32) (*catModel
 			return nil, err
 		}
 
-		go func() {
-			_, err := rds.TxPipelined(ctx, func(pipe redis.Pipeliner) error {
-				var err error
-				err = pipe.HSet(ctx, keys.ProductKey(productId), resp /* resp to product */).Err()
-				if err != nil {
-					return err
-				}
-				err = pipe.Expire(ctx, keys.ProductKey(productId), 10*time.Minute).Err() // Global expiry on the key.
-				return err
-			})
-			if err != nil {
-				global.Logger().Error("Error setting product cache", zap.Error(err))
-			}
-		}()
+		go SetProduct(ctx, api, productId, resp)
 	}
 
 	return product, nil
+}
+
+func SetProduct(ctx context.Context, api *types.Api, productId uint32, data *catalog.GetProductResponse) error {
+	rds := api.Deps.Redis
+	_, err := rds.TxPipelined(ctx, func(pipe redis.Pipeliner) error {
+		var err error
+		err = pipe.HSet(ctx, keys.ProductKey(productId), data /* resp to product */).Err()
+		if err != nil {
+			return err
+		}
+		err = pipe.Expire(ctx, keys.ProductKey(productId), 10*time.Minute).Err() // Global expiry on the key.
+		return err
+	})
+	if err != nil {
+		global.Logger().Error("Error setting product cache", zap.Error(err))
+	}
+	return err
 }

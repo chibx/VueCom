d3db5bc0078caa98af0a64dbb80299a337452d2d
chibx <111882496+chibx@users.noreply.github.com>
2026-04-19 01:04:27 +0100

    Register additional defined inventory route handlers

--- backend/services/gateway/api/v1/handlers/backend/inventory/register.go
+++ backend/services/gateway/api/v1/handlers/backend/inventory/register.go
@@ -11,4 +11,8 @@ func RegisterRoutes(app fiber.Router, api *types.Api) {
 
 	inventoryGroup.Get("/warehouses/exist", HasAnyWarehouse(api))
 	inventoryGroup.Get("/warehouses", ListWarehouses(api))
+	inventoryGroup.Post("/warehouses", CreateWarehouse(api))
+	inventoryGroup.Delete("/warehouses", DeleteWarehouse(api))
+	inventoryGroup.Post("/stock-movements", CreateStockMovement(api))
+	inventoryGroup.Get("/stock-movements", ListStockMovements(api))
 }
